6b1b6161b4164c53fb216b098e3fd8a873ea5b15
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-09-11 11:52:52 -0700

    Provider, store, reducer started

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+function reducer() {
+    return {
+      title: 'Car Sales'
+    }
+  }
+  
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, rootElement);
