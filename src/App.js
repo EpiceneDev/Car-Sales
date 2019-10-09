897a1a98fab4a6377edf621e5fd2b0fa2160ad2f
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-09 12:54:55 -0700

    restart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {dispatch} from 'react';
+import { connect } from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
@@ -6,21 +7,31 @@ import Total from './components/Total';
 
 
 
-const App = () => {
+const App = (props) => {
+  const { state, removeFeature } = props
+ 
  
   const removeFeature = item => {
-    // dispatch an action here to remove an item
+    return {
+      type: "REMOVE_FEATURES",
+      payload: item
+    }
+  };
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    dispatch({
+      type: "BUY_ITEM",
+      state.car.features: action.payload
+    })
   };
 
   return (
     <div className="boxes">
       <div className="box">
         <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <AddedFeatures car={state.car} removeFeature={removeFeature} />
       </div>
       <div className="box">
         <AdditionalFeatures store={state.store} />
@@ -30,4 +41,8 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {state};
+}
+
+export default connect (mapStateToProps, {buyItem, removeFeature})(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import {reducer} from './reducer';
 
 import App from './App';
 
@@ -8,8 +10,9 @@ import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
+export const store = createStore(reducer);
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
     <App />
 </Provider>, rootElement);
